66db2c126ca5f97c4b523cc23bb29bef9bab3667
KimBoris <zlas2345@gmail.com>
2024-08-19 18:50:47 +0900

    Test

---
+++ src/components/checkout/BacktoTop.vue
@@ -0,0 +1,16 @@
+<template>
+
+<a href="#" class="btn btn-primary back-to-top" style="display: inline;"><i class="fa fa-angle-double-up"></i></a>
+
+
+</template>
+
+
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/page/ShopPage.vue
@@ -0,0 +1,25 @@
+<template>
+
+ <Topbar></Topbar>
+ <navbar></navbar>
+<PageHeader></PageHeader>
+<Checkout></Checkout>
+<Footer></Footer>
+<BacktoTop></BacktoTop>
+
+</template>
+
+<script setup>
+import BacktoTop from '@/components/checkout/BacktoTop.vue';
+import Checkout from '@/components/checkout/Checkout.vue';
+import Footer from '@/components/checkout/Footer.vue';
+import Navbar from '@/components/checkout/Navbar.vue';
+import PageHeader from '@/components/checkout/PageHeader.vue';
+import Topbar from '@/components/checkout/Topbar.vue';
+
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
